83db44dbf7941974917447b88d557a74b8bf13f5
TheCodeStacker <thecodestacker@gmail.com>
2025-11-26 19:43:56 +0700

    Create copyright.js

---
+++ js/components/copyright.js
@@ -0,0 +1,7 @@
+export function renderCopyright() {
+	document.getElementById('copyright').innerHTML = `
+		<div class="mt-8 text-center text-sm text-gray-500">
+			<p>© 2025 CodeStacker. All rights reserved.</p>
+		</div>
+	`;
+}
